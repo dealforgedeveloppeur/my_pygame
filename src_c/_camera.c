b439f40cf3933a3fab72ee9284f1bd4cf49055c5
dealforgedeveloppeur <dealforge.developpeur@gmail.com>
2026-08-14 15:26:52 +0200

    Rename camera type from pygame to my_pygame

--- src_c/_camera.c
+++ src_c/_camera.c
@@ -1757,7 +1757,7 @@ camera_init(pgCameraObject *self, PyObject *arg, PyObject *kwargs)
 }
 
 PyTypeObject pgCamera_Type = {
-    PyVarObject_HEAD_INIT(NULL, 0).tp_name = "pygame.camera.Camera",
+    PyVarObject_HEAD_INIT(NULL, 0).tp_name = "my_pygame.camera.Camera",
     .tp_basicsize = sizeof(pgCameraObject),
     .tp_dealloc = camera_dealloc,
     .tp_doc = DOC_PYGAMECAMERACAMERA,
